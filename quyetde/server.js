8a0d9f70388e4ae05d155a0ec6e64100bbff7875
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2019-03-13 08:54:04 +0700

    A

--- quyetde/server.js
+++ quyetde/server.js
@@ -9,18 +9,51 @@ app.get("/", (req, res) => {
 	//TODO:
 	// - Lấy 1 câu hỏi ngẫu nhiên từ file dùng để lưu câu hỏi
 	// - Hiển thị ra màn hình như trang mẫu http://quyetde123.herokuapp.com/
-})
+	// const questionList = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
+	// const randomQuestion = questionList[Math.floor(Math.random()*questionList.length)];
+	// res.send(`<h1>
+	// 	${randomQuestion}
+	// </h1>`);
+	res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/randomquestion", (req, res) => {
+	const questionList = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
+	const randomQuestion = questionList[Math.floor(Math.random()*questionList.length)];
+	res.send(randomQuestion);
+});
 
 app.get("/ask", (req, res) => {
 	res.sendFile(__dirname + "/views/ask.html");
 });
 
-app.post("/addquestion", (req, res) => {
-	const question = req.body.question;
+app.put("/editquestion", (req, res) => {
 	const questionList = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
+	const question = req.body;
+	questionList[question.id] = question;
+	fs.writeFileSync("./questions.json", JSON.stringify(questionList));
+});
+
+app.post("/addquestion", (req, res) => {
+	const questionList = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
+	const question = {
+		content: req.body.question,
+		yes: 0,
+		no: 0,
+		id: questionList.length,
+	};
 	questionList.push(question);
 	fs.writeFileSync("./questions.json", JSON.stringify(questionList));
 	res.redirect("/ask");
+});
+
+app.get("/vote/:questionId/yes", (req, res) => {
+	const questionList = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
+	const questionId = req.params.questionId;
+	questionList[questionId].yes = Number(questionList[questionId].yes) + 1;
+	// console.log(questionList);
+	fs.writeFileSync("./questions.json", JSON.stringify(questionList));
+	res.send();
 });
 
 // app.post("/", (req, res) => {
